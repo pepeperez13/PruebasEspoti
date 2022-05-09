94615b7bf1e893f836f68acb3efa5d4f5d795967
pepeperez13 <josepehern@gmail.com>
2022-05-10 00:36:19 +0200

    lala

--- src/DetailedSongView.java
+++ src/DetailedSongView.java
@@ -1,5 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class DetailedSongView extends JFrame {
+
+
+    public DetailedSongView () {
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(1500, 900);
+        setResizable(false);
+
+        setLayout(new BorderLayout(0, 50));
+
+        add(setTitleAndAuthor("The Time", "The Black Eyed Peas"), BorderLayout.NORTH);
+
+        setVisible(true);
+    }
+
+    private JLabel setTitleAndAuthor (String title, String author) {
+        JLabel text = new JLabel(title + " - " + author);
+        text.setPreferredSize(new Dimension(100, 60));
+        text.setFont(new Font("Tahoma", Font.BOLD, 24));
+        text.setHorizontalAlignment(JLabel.CENTER);
+        text.setBackground(new Color(52, 166, 244));
+        text.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+        text.setFocusable(false);
+        return text;
+    }
 
 }
